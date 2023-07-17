0bd1fb2920ef3e420bfc5fe0776944da8ef93023
Yevhen Herasimov <yevhen.gerasimov@gmail.com>
2023-07-17 12:15:48 +0300

    Updated prepare.py to handle variable unique dataset characters

--- data/shakespeare_char/prepare.py
+++ data/shakespeare_char/prepare.py
@@ -18,47 +18,58 @@ if not os.path.exists(input_file_path):
 
 with open(input_file_path, 'r') as f:
     data = f.read()
-print(f"length of dataset in characters: {len(data):,}")
 
-# get all the unique characters that occur in this text
-chars = sorted(list(set(data)))
-vocab_size = len(chars)
-print("all the unique characters:", ''.join(chars))
-print(f"vocab size: {vocab_size:,}")
 
-# create a mapping from characters to integers
-stoi = { ch:i for i,ch in enumerate(chars) }
-itos = { i:ch for i,ch in enumerate(chars) }
 def encode(s):
-    return [stoi[c] for c in s] # encoder: take a string, output a list of integers
+    return [stoi[c] if c in chars else 0 for c in s]  # encoder: take a string, output a list of integers
+
+
 def decode(l):
-    return ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
+    return ''.join([itos[i] for i in l])  # decoder: take a list of integers, output a string
 
-# create the train and test splits
-n = len(data)
-train_data = data[:int(n*0.9)]
-val_data = data[int(n*0.9):]
+# get all unique characters from the smallest dataset slice
+chars = sorted(list(set(data[:int(len(data) * 0.1)])))
 
-# encode both to integers
-train_ids = encode(train_data)
-val_ids = encode(val_data)
-print(f"train has {len(train_ids):,} tokens")
-print(f"val has {len(val_ids):,} tokens")
+for slice_size in np.arange(0.9, 0, -0.1):
+    temp_data = data[:int(len(data) * slice_size)]
+    temp_dataset_len = len(temp_data)
+    print(f"length of dataset in characters: {temp_dataset_len}")
 
-# export to bin files
-train_ids = np.array(train_ids, dtype=np.uint16)
-val_ids = np.array(val_ids, dtype=np.uint16)
-train_ids.tofile(os.path.join(os.path.dirname(__file__), 'train.bin'))
-val_ids.tofile(os.path.join(os.path.dirname(__file__), 'val.bin'))
+    # get all the unique characters that occur in this text
+    # chars = sorted(list(set(temp_data)))
+    vocab_size = len(chars)
+    print("all the unique characters:", ''.join(chars))
+    print(f"vocab size: {vocab_size:,}")
 
-# save the meta information as well, to help us encode/decode later
-meta = {
-    'vocab_size': vocab_size,
-    'itos': itos,
-    'stoi': stoi,
-}
-with open(os.path.join(os.path.dirname(__file__), 'meta.pkl'), 'wb') as f:
-    pickle.dump(meta, f)
+    # create a mapping from characters to integers
+    stoi = {ch: i for i, ch in enumerate(chars)}
+    itos = {i: ch for i, ch in enumerate(chars)}
+
+    # create the train and test splits
+    train_data = temp_data[:int(temp_dataset_len * 0.9)]
+    val_data = temp_data[int(temp_dataset_len * 0.9):]
+
+    # encode both to integers
+    train_ids = encode(train_data)
+    val_ids = encode(val_data)
+    print(f"train has {len(train_ids):,} tokens")
+    print(f"val has {len(val_ids):,} tokens")
+
+    # export to bin files
+    train_ids = np.array(train_ids, dtype=np.uint16)
+    val_ids = np.array(val_ids, dtype=np.uint16)
+    train_ids.tofile(os.path.join(os.path.dirname(__file__), f'train{int(slice_size*100)}.bin'))
+    val_ids.tofile(os.path.join(os.path.dirname(__file__), f'val{int(slice_size*100)}.bin'))
+
+    # save the meta information as well, to help us encode/decode later
+    meta = {
+        'vocab_size': vocab_size,
+        'itos': itos,
+        'stoi': stoi,
+    }
+
+    with open(os.path.join(os.path.dirname(__file__), f'meta{int(slice_size*100)}.pkl'), 'wb') as f:
+        pickle.dump(meta, f)
 
 # length of dataset in characters:  1115394
 # all the unique characters:
